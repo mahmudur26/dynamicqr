4be87cd699861054712d3b95bc1b5e0661aac6eb
Aurko Home <mahmudur.rashid@northsouth.edu>
2023-02-07 00:37:54 +0600

    admin home page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,15 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
 
-class AdminController extends Controller
+class AdminController extends BaseController
 {
     public function home()
     {
+        // $data['title'] = 'Home Page';
+        // $data['view_page'] = 'admin/admin-home';
         $data['users'] = User::where('is_verified' , '=' , NULL)->get();
-        // dd($data['users']);
-        $data['title'] = 'Home Page';
-        $data['view_page'] = 'admin/admin-home';
-        return view('admin/frame' , $data);
+        return view("admin.admin-home" , $data);
     }
 }
--- resources/views/admin/admin-home.blade.php
+++ resources/views/admin/admin-home.blade.php
@@ -1,20 +1,78 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Bootstrap demo</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row">
+        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+        <div class="position-sticky pt-3 sidebar-sticky">
+            <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                <span data-feather="home" class="align-text-bottom"></span>
+                Admin Dashboard
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                <span data-feather="file" class="align-text-bottom"></span>
+                Orders
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                <span data-feather="shopping-cart" class="align-text-bottom"></span>
+                Products
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                <span data-feather="users" class="align-text-bottom"></span>
+                Customers
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                <span data-feather="bar-chart-2" class="align-text-bottom"></span>
+                Reports
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                <span data-feather="layers" class="align-text-bottom"></span>
+                Integrations
+                </a>
+            </li>
+            </ul>
+        </div>
+        </nav>
+    
+        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">Admin Dashboard</h1>
+        </div>
+
+        
 <h2>Pending Users</h2>
 <div class="table-responsive">
     <table class="table table-striped table-sm">
     <thead>
         <tr>
         <th scope="col">#</th>
-        <th scope="col">Name</th>
         <th scope="col">Email</th>
         <th scope="col">Phone</th>
         <th scope="col">View</th>
         </tr>
     </thead>
     <tbody>
-        @foreach ($users as $key => $user)
+        @foreach ($users as $user)
         <tr>
             <td>{{$user->id}}</td>
-            <td>{{$user->name}}</td>
             <td>dat{{$user->email}}a</td>
             <td>{{$user->phone}}</td>
             <td>View</td>
@@ -22,4 +80,12 @@
         @endforeach
     </tbody>
     </table>
+</div>   
+</main>
 </div>
+</div>
+    
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>
