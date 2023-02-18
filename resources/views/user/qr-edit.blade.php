ab62d109f666c83d308b199ddd3fbb096aa660b0
Aurko Home <mahmudur.rashid@northsouth.edu>
2023-02-18 19:59:55 +0600

    back to list button added in edit qr section

--- resources/views/user/qr-edit.blade.php
+++ resources/views/user/qr-edit.blade.php
@@ -9,7 +9,10 @@ form{
     margin-right: auto;
     border-radius: 15px;
     box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
-    padding: 20px 15px 60px 15px;
+    padding: 20px 15px 40px 15px;
+}
+.back_button{
+    margin-right: 5px;
 }
 </style>
 
@@ -21,7 +24,8 @@ form{
             <label for="url_text">URL</label>
             <input type="text" class="form-control" name="updated_url" id="url_text" value="{{$qr->input_text}}">
         </div>
-        <button type="submit" class="btn btn-success pull-right">Change URL</button>
+            <button type="submit" class="btn btn-success">Change URL</button>
+            <a class="btn btn-primary back_button" href="{{route('qr-list')}}">Back to List</a>
     </form>
 </div>
 
