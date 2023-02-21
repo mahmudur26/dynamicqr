a2f38305881dbb23bea339ea148e710e5b15beab
Aurko-Laptop2 <aurko.nsu.bd@gmail.com>
2023-02-21 23:20:32 +0600

    sending email to registered user

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Exception;
 use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Mail\VerificationMail;
+use Illuminate\Support\Facades\Mail;
 
 class RegisterController extends Controller
 {
@@ -23,13 +25,32 @@ class RegisterController extends Controller
             'password' => 'required|confirmed|min:5',
         ]);
 
+        $token = Str::random(6);
+        $user_email = strtolower($request->input('email'));
+
         $user = User::create([
             'email' => strtolower($request->input('email')),
             'phone' => strtolower($request->input('phone')),
             'password' => bcrypt($request->input('password')),
-            'email_verification_token' => Str::random(6),
+            'email_verification_token' => $token,
         ]);
+        $this->sendProfileVerifyMailTo($user_email , $token);
         session()->flash('message', 'Please check your email to activate your account');
         return redirect()->back();
+    }
+
+    public function sendProfileVerifyMailTo($email , $code)
+    {
+        $data = [
+            "subject"=>"Verify your Email",
+            "code" => $code
+            ];
+          try
+          {
+            Mail::to($email)->send(new VerificationMail($data));
+          }
+          catch(Exception $e)
+          {
+          }
     }
 }
